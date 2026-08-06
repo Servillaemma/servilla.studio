875ba311affd6e5e49cb4617ce017616e67c5c4e
Emma Servilla <emmaservilla@Mac.lan>
2026-08-06 20:56:55 +0200

    Fix direct About page routing on Vercel

--- vercel-main.tsx
+++ vercel-main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./app/page";
+import AboutPage from "./app/a-propos/page";
 import "./app/globals.css";
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
+const Page = normalizedPath === "/a-propos" ? AboutPage : Home;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <Page />
   </StrictMode>,
 );
